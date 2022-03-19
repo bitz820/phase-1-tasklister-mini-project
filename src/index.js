695bc12bd77428f0dfadecee65f7621228bc35d8
Mark <mark.n.bacon.jr@gmail.com>
2022-03-19 15:56:27 -0400

    this works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let taskList = document.querySelector('#list')
+  let form = document.querySelector('form')
+
+  // Listen for a click event on the form and prevent default
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+  // select the words typed into the input box
+    const toDo = e.target.new_task_description.value
+    // Append ToDo to List
+    let li = document.createElement('li')
+    li.textContent = toDo
+    console.log(li)
+    // select the ToDo List
+    let toDoList = document.querySelector("#tasks")
+    toDoList.append(li)
+
+  });
+  
+})
+
+
+// Type a task into input field
